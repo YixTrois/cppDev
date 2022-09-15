6789a0600637e3474698dca8ea3c36b038c27214
YixTrois <yixingma.c.3@gmail.com>
2022-09-15 16:29:48 -0500

    cpp on mac is a pain

--- ClassesAndObjects.cpp
+++ ClassesAndObjects.cpp
@@ -3,10 +3,9 @@
 
 int main()
 {
-    std::cout<<"helllo";
-    // Person p1;
-    // Person p2;
-    // std::string name = p1.getName();
+    Person p1;
+    Person p2;
+    std::string name = p1.getName();
 
     //int i = p1.arbitrarynumber;
 
